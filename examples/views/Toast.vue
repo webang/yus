7c31b9cc76ec2f1f1eaccf75a01f196b3888f1d3
Arraybuffer <arraybuffer@foxmail.com>
2018-11-07 17:15:53 +0800

    add: Toast Component

--- examples/views/Toast.vue
+++ examples/views/Toast.vue
@@ -1,9 +1,126 @@
 <template>
-  <div class="page-toast"></div>
+  <div class="page-toast">
+    <div class="ymu-doc-block">
+      <div class="ymu-doc-block__title">基础用法-position</div>
+      <div class="ymu-doc-block__content">
+        <CellGroup>
+          <Cell title="middle">
+            <span slot="value" @click="show">显示</span>
+          </Cell>
+          <Cell title="top">
+            <span slot="value" @click="show2">显示</span>
+          </Cell>
+          <Cell title="bottom">
+            <span slot="value" @click="show3">显示</span>
+          </Cell>
+        </CellGroup>
+      </div>
+    </div>
+    <div class="ymu-doc-block">
+      <div class="ymu-doc-block__title">设置-type</div>
+      <div class="ymu-doc-block__content">
+        <CellGroup>
+          <Cell title="success">
+            <span slot="value" @click="show4">显示</span>
+          </Cell>
+          <Cell title="fail">
+            <span slot="value" @click="show5">显示</span>
+          </Cell>
+          <Cell title="warn">
+            <span slot="value" @click="show6">显示</span>
+          </Cell>
+          <Cell title="error">
+            <span slot="value" @click="show7">显示</span>
+          </Cell>
+        </CellGroup>
+      </div>
+    </div>
+    <div class="ymu-doc-block">
+      <div class="ymu-doc-block__title">不自己关闭-autoClose</div>
+      <div class="ymu-doc-block__content">
+        <CellGroup>
+          <Cell title="false">
+            <Button slot="value" size="mini" type="primary" plain @click="show8">切换</Button>
+          </Cell>
+        </CellGroup>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import Toast from '@/components/toast'
+import Cell from '@/components/cell'
+import CellGroup from '@/components/cell-group'
+import Button from '@/components/button'
 
+export default {
+  components: {
+    Cell,
+    Button,
+    CellGroup
+  },
+  data () {
+    return {
+      demo1: {
+        position: 'middle'
+      }
+    }
+  },
+  methods: {
+    show () {
+      Toast({
+        message: '请勿重复提交订单'
+      })
+    },
+    show2 () {
+      Toast({
+        position: 'top',
+        message: '请勿重复提交订单'
+      })
+    },
+    show3 () {
+      Toast({
+        position: 'bottom',
+        message: '请勿重复提交订单'
+      })
+    },
+    show4 () {
+      Toast({
+        type: 'success',
+        message: '操作成功'
+      })
+    },
+    show5 () {
+      Toast({
+        type: 'fail',
+        message: '操作失败'
+      })
+    },
+    show6 () {
+      Toast({
+        type: 'warn',
+        message: '操作失败'
+      })
+    },
+    show7 () {
+      Toast({
+        type: 'error',
+        message: '服务器出错'
+      })
+    },
+    show8 () {
+      if (this.toast) {
+        Toast.clear(this.toast)
+        this.toast = null
+        return
+      }
+      this.toast = Toast({
+        type: 'error',
+        message: '服务器出错',
+        autoClose: false
+      })
+    }
+  }
 }
 </script>
--- src/components/toast/src/toast.vue
+++ src/components/toast/src/toast.vue
@@ -1,13 +1,73 @@
 <template>
-  <div class="ymu-toast">
-
-  </div>
+  <transition :name="transition">
+    <div
+      v-show="visible"
+      class="ymu-toast"
+      :class="[
+        'ymu-toast--'+type,
+        'ymu-toast--'+position
+      ]"
+    >
+      <div class="ymu-toast-content">
+        <slot>
+          <slot name="icon">
+            <div class="ymu-toast__icon-wrap" v-if="hasIcon">
+              <i class="iconfont ymu-toast__icon" :class="['icon-'+type]"></i>
+            </div>
+          </slot>
+          <span class="ymu-toast__text">{{ message }}</span>
+        </slot>
+      </div>
+    </div>
+  </transition>
 </template>
 
 <script>
 export default {
-
+  name: 'ymu-toast',
+  props: {
+    value: Boolean,
+    message: String,
+    type: {
+      type: String,
+      default: 'text'
+    },
+    duration: {
+      type: Number,
+      default: 2000
+    },
+    position: {
+      type: String,
+      default: 'middle'
+    },
+    speed: {
+      type: Number,
+      default: 400
+    },
+    autoClose: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data () {
+    return {
+      visible: false
+    }
+  },
+  computed: {
+    transition () {
+      return 'ymu-toast--' + this.position
+    },
+    hasIcon () {
+      return this.type !== 'text'
+    }
+  },
+  watch: {
+    value (val) {
+      this.visible = val
+    }
+  }
 }
 </script>
 
-<style lang="scss" src="./src/toast.scss"></style>
+<style lang="scss" src="./toast.scss"></style>
