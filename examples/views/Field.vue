00d273e3bac736c986ff90f7085aa2b5e31e0a11
gaollard <gaollard@foxmail.com>
2018-11-07 23:03:53 +0800

    update

--- examples/views/Field.vue
+++ examples/views/Field.vue
@@ -2,17 +2,32 @@
   <div class="page-field">
     <CellGroup title="基础用法">
       <Field label="账户" placeholder="请输入账户" v-model="demo1.v1"></Field>
-      <Field placeholder="请输入密码" v-model="demo1.v2"></Field>
     </CellGroup>
-    <CellGroup title="设置type">
-      <Field label="text" placeholder="请输入账户" type="text" v-model="demo2.v1"></Field>
+    <CellGroup title="自定义类型">
       <Field label="number" placeholder="请输入账户" type="number" v-model="demo2.v2"></Field>
       <Field label="password" placeholder="请输入密码" type="password" v-model="demo2.v3"></Field>
     </CellGroup>
-    <CellGroup title="设置clear">
-      <Field label="text" placeholder="请输入账户" type="text" clear></Field>
-      <Field label="number" placeholder="请输入账户" type="number" clear></Field>
-      <Field label="password" placeholder="请输入密码" type="password" clear></Field>
+    <CellGroup title="显示清除按钮">
+      <Field label="true" placeholder="请输入账户" type="number" clearable v-model="demo2.v2"></Field>
+    </CellGroup>
+    <CellGroup title="禁用输入框">
+      <Field label="true" placeholder="请输入账户" type="text" disabled></Field>
+    </CellGroup>
+    <CellGroup title="长度限制">
+      <Field
+        label="手机号码"
+        placeholder="请输入手机号码"
+        :min="5"
+        :max="10"
+        v-model="demo2.v3"
+        type="number"></Field>
+    </CellGroup>
+    <CellGroup title="设置错误提示">
+      <Field
+        label="手机号码"
+        errorMessage="手机号码格式不正确"
+        placeholder="请输入手机号码"
+        type="number"></Field>
     </CellGroup>
   </div>
 </template>
@@ -55,8 +70,4 @@ export default {
 </script>
 
 
-<style lang="scss" scoped>
-.ymu-doc-block {
-  /*background-color: #fff;*/
-}
-</style>
+<style lang="scss" scoped></style>
--- src/components/field/src/field.vue
+++ src/components/field/src/field.vue
@@ -2,15 +2,30 @@
   <div class="ymu-field">
     <div class="ymu-field__hd">
       <slot name="header">
-        <label class="ymu-field__label" v-if="label">{{ label }}</label>
+        <label class="ymu-field__label" v-if="label" v-text="label"></label>
       </slot>
     </div>
     <div class="ymu-field__bd">
-      <input class="ymu-field__input" :placeholder="placeholder" :type="type" v-model="currentValue">
+      <input
+        class="ymu-field__input"
+        :placeholder="placeholder"
+        :type="type"
+        :value="value"
+        :disabled="disabled"
+        :readonly="readonly"
+        maxlength="5"
+        v-on="listeners"
+      >
+      <div
+        v-if="errorMessage"
+        v-text="errorMessage"
+        class="ymu-field__error-message"></div>
     </div>
     <div class="ymu-field__ft">
-      <!-- <i class="ymu-icon ymu-icon-clear iconfont icon-clear" v-if="currentValue && clear" @click="onClear"></i> -->
-      <i class="ymu-icon ymu-icon-clear" v-if="currentValue && clear" @click="onClear"></i>
+      <i
+        class="ymu-icon ymu-icon-clear"
+        v-if="showClear"
+        @click="onClear"></i>
       <slot name="footer"></slot>
     </div>
   </div>
@@ -23,13 +38,29 @@ export default {
     label: String,
     placeholder: String,
     value: [Number, String],
+    clearable: Boolean,
+    disabled: Boolean,
+    readonly: Boolean,
+    errorMessage: String,
     type: {
       type: String,
       default: 'text'
     },
-    clear: {
-      type: Boolean,
-      default: true
+    min: Number,
+    max: Number
+  },
+  computed: {
+    showClear () {
+      return this.clearable && this.value !== '' && !this.readonly
+    },
+    listeners () {
+      return {
+        ...this.$listeners,
+        input: this.onInput,
+        keypress: this.onKeypress,
+        focus: this.onFocus,
+        blur: this.onBlur
+      }
     }
   },
   data () {
@@ -53,6 +84,26 @@ export default {
   methods: {
     onClear () {
       this.currentValue = ''
+    },
+    format (target) {
+      const { value } = target
+      let formatValue = value
+      if (this.max && this.max < value.length) {
+        target.value = formatValue = value.slice(0, this.max)
+      }
+      return formatValue
+    },
+    onInput (event) {
+      this.$emit('input', this.format(event.target))
+    },
+    onBlur (event) {
+      console.log(event)
+    },
+    onKeypress (event) {
+      console.log(event)
+    },
+    onFocus (event) {
+      console.log(event)
     }
   }
 }
