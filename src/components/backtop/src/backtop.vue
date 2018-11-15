12e3c4198c7352faddfb9b84cc764bbfa39bab08
Arraybuffer <arraybuffer@foxmail.com>
2018-11-15 17:52:05 +0800

    add: Backtop Component

--- src/components/backtop/src/backtop.vue
+++ src/components/backtop/src/backtop.vue
@@ -1,13 +1,32 @@
 <template>
-  <div class="ym-backtop">
-
+  <div class="ym-backtop" @click="handleClick">
+    <i class="ym-icon icon-backtop"></i>
+    <span class="ym-backtop__text">顶部</span>
   </div>
 </template>
 
 <script>
+/*
+ * t: current time（当前时间）；
+ * b: beginning value（初始值）；
+ * c: change in value（变化量）；
+ * d: duration（持续时间
+*/
+const moveJS = {
+  linear: function (t, b, c, d) {
+    return c * t / d + b
+  }
+}
+
 export default {
-  name: 'ym-backtop'
+  name: 'ym-backtop',
+  methods: {
+    handleClick () {
+      // 此处还缺算法哦，要先去复习动画相关的算法了
+      document.documentElement.scrollTop = 0
+    }
+  }
 }
 </script>
 
-<style src="./backtop.scss"></style>
+<style lang="scss" src="./backtop.scss"></style>
