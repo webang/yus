d9f36e7a8fc4134c8c57ac99547d64cde8a5270e
Arraybuffer <arraybuffer@foxmail.com>
2018-12-05 21:04:58 +0800

    update

--- src/components/radio-group/src/radio-group.vue
+++ src/components/radio-group/src/radio-group.vue
@@ -1,1 +1,16 @@
-radio-group.vue
+<template>
+  <div class="ymu-radio-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'radio-group',
+  props: {
+    value: [String, Number, Boolean]
+  }
+}
+</script>
+
+<style lang="scss" src="./radio-group.scss"></style>
