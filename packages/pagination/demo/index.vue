34cf5a8b678d6cfa1917274bba5d3620b4949b4b
gaollard <gaollard@foxmail.com>
2019-03-31 15:51:51 +0800

    add Pagination

--- packages/pagination/demo/index.vue
+++ packages/pagination/demo/index.vue
@@ -2,7 +2,12 @@
   <div class="demo-pagination">
     <div class="ymu-doc-block__hd">默认</div>
     <div class="ymu-doc-block__bd">
-      <ymu-pagination :total=30 :pageIndex=pageIndex :pageSize=5 @on-index-change=onIndexChange />
+      <ymu-pagination
+        :total=300
+        :pageIndex=pageIndex
+        :pageSize=5
+        :perSize=5
+        @on-index-change=onIndexChange />
     </div>
   </div>
 </template>
@@ -27,38 +32,7 @@ export default {
 .ymu-doc-block__bd {
   background: #fff;
 }
-
-.menu {
-  width: 100px;
-  line-height: 40px;
-  li {
-    font-size: 14px;
-    color: #666;
-    text-align: center;
-    border-bottom: 1px #ffeeff solid;
-  }
-}
-
-.demo-01,
-.demo-02 {
-  .trigger {
-    display: block;
-    height: 40px;
-    line-height: 40px;
-    color: #999;
-  }
-}
-
-.demo-04,
-.demo-03  {
-  .trigger {
-    display: block;
-    width: 50px;
-    height: 40px;
-    line-height: 40px;
-    margin: 0 auto;
-    color: #999;
-    text-align: center;
-  }
+/deep/ .ymu-pagination {
+  margin: 0 auto;
 }
 </style>
--- packages/pagination/index.vue
+++ packages/pagination/index.vue
@@ -3,19 +3,41 @@
     <a
       class="ymu-pagination-item"
       href="javascript:;void(0)"
-      :class="{'ymu-pagination-item--disabled': pageIndex <= 1}">上一页</a>
+      @click="updateIndex(pageIndex - 1)"
+      :class="{'ymu-pagination-item--disabled': pageIndex <= 1}">&lt;</a>
+    <a
+      v-if="showPrevDot"
+      class="ymu-pagination-item"
+      href="javascript:;void(0)"
+      @click="updateIndex(1)">1</a>
+    <a
+      v-if="showPrevDot"
+      class="ymu-pagination-item"
+      href="javascript:;void(0)"
+      @click="updateIndex(pageIndex - perSize)">...</a>
     <div
       class="ymu-pagination-item"
       :class="{'ymu-pagination-item--active': item===pageIndex}"
-      v-for="item in count" :key="item"
-      @click="handleClick(item)"
+      v-for="item in curList" :key="item"
+      @click="updateIndex(item)"
     >
       <span> {{ item }}</span>
     </div>
     <a
+      v-if="showNextDot"
       class="ymu-pagination-item"
       href="javascript:;void(0)"
-      :class="{'ymu-pagination-item--disabled': pageIndex >= count}">下一页</a>
+      @click="updateIndex(pageIndex + perSize)">...</a>
+    <a
+      v-if="pageIndex <= pageNum - perSize"
+      class="ymu-pagination-item"
+      href="javascript:;void(0)"
+      @click="updateIndex(pageNum)">{{ pageNum }}</a>
+    <a
+      class="ymu-pagination-item"
+      href="javascript:;void(0)"
+      @click="updateIndex(pageIndex + 1)"
+      :class="{'ymu-pagination-item--disabled': pageIndex >= pageNum}">&gt;</a>
   </div>
 </template>
 
@@ -57,14 +79,70 @@ export default useName({
     }
   },
   computed: {
-    count () {
-      return this.total / this.pageSize
+    showNextDot () {
+      return this.pageIndex + this.perSize <= this.pageNum;
+    },
+    showPrevDot () {
+      return this.pageIndex >= this.perSize;
+    },
+    pageNum () {
+      return this.total / this.pageSize;
+    },
+    curList () {
+      if (this.pageNum <= this.perSize) {
+        /**
+         * 当总页数小于分组大小时，展示所有的页码
+         * @example pageNum = 3 perSize = 5 return [1, 2, 3]
+         */
+        const list = [];
+        for (let index = 0; index < this.pageNum; index++) {
+          list.push(index + 1);
+        }
+        return list;
+      } else {
+       /**
+         * 当总页数小于大于大小
+         * @example pageNum = 20 perSize = 5
+         */
+        if (this.pageIndex <= (this.perSize - 1) / 2) {
+          /**
+           * 当前索引不大于分组大小的一半
+           * @example perSize = 5 pageIndex = 1 2 => [1, 2, 3, 4, 5]
+           */
+          const list = [];
+          for (let index = 0; index < this.perSize; index++) {
+            if (index + 1 <= this.pageNum) {
+              list.push(index + 1);
+            }
+          }
+          return list;
+        } else if (this.pageIndex + (this.perSize - 1) / 2 >= this.pageNum) {
+          const list = [];
+          for (let index = 0; index < this.perSize; index++) {
+            list.push(this.pageNum - this.perSize + index + 1);
+          }
+          return list;          
+        } else {
+          const list = [this.pageIndex];
+          for (let index = 0; index < (this.perSize - 1) / 2; index++) {
+            if (this.pageIndex + index + 1 <= this.pageNum) {
+              list.push(this.pageIndex + index + 1);
+            }
+          }
+          for (let index = 0; index < (this.perSize - 1) / 2; index++) {
+            list.unshift(this.pageIndex - (index + 1));
+          }
+          return list;    
+        }
+      }
     }
   },
   methods: {
     handleClick (index) {
       console.log(index);
-      this.$emit('on-index-change', index);
+    },
+    updateIndex (value) {
+      this.$emit('on-index-change', value);
     }
   }
 })
